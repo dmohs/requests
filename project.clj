69deda89168295b38b6f24ed4dc420972ff52fda
David Mohs <dgit@mohsinc.com>
2016-04-25 12:16:05 -0400

    Add initial project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dmohs/requests "0.0.1" ;; Also update README.md on version change.
+  :description "A request-handling library for Node-based ClojureScript HTTP servers."
+  :license "http://opensource.org/licenses/MIT"
+  :url "https://github.com/dmohs/requests"
+  :source-paths ["src/cljs/core"]
+  )
