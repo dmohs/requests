78a4be1915e77c87fafbb317f71faf3e7969e8a0
David Mohs <dgit@mohsinc.com>
2016-07-27 18:53:33 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dmohs/requests "0.0.1" ;; Also update README.md on version change.
+(defproject dmohs/requests "0.0.2" ;; Also update README.md on version change.
   :description "A request-handling library for Node-based ClojureScript HTTP servers."
   :license "http://opensource.org/licenses/MIT"
   :url "https://github.com/dmohs/requests"
